63eefcace08661b23fea4b63dd19761b1c1ed2d0
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-04-07 02:20:47 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
-#include"test/catch.hpp"
-#include"Sender.h"
-#include"Sender.c"
+#include "test/catch.hpp"
+#include "Sender.h"
+#include "Sender.c"
 
 TEST_CASE("Tests to check if all the data is correctly read from file") {
  REQUIRE (PrintParameterToConsole() == 1)
