e0da120cd6db18c518b85d181ee49f52c716a00d
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-04-07 02:31:05 +0530

    Update Sender.h

--- Sender.h
+++ Sender.h
@@ -1,5 +1,5 @@
 #define NO_OF_READINGS 50
-void print(floattemperature, floatSOC);
+void print(float Temperature, float SOC);
 void ReadParametersFromFile(float* Temperature, float* SOC);
 int PrintParameterToConsole(float* Temperature, float* SOC);
 void ParametersSender();
