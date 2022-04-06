2763b3080c8be8fb5ef31cbb8d69be7e3dc47a3a
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-04-07 02:45:40 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "Sender.h"
+#include "Sender.c"
+
+TEST_CASE("Tests to check if all the data is correctly read from file") {
+ REQUIRE (PrintParameterToConsole() == 1)
+  }
