1a0573c5ba4c5a61845a9dbc976d8754b113fda4
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-04-07 11:24:34 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,6 +4,19 @@
 #include "Sender.h"
 #include "Sender.c"
 
-TEST_CASE("Tests to check if all the data is correctly read from file") {
- REQUIRE (PrintParameterToConsole() == 1)
+  float Temperature[NO_OF_READINGS] = {20,25,45};
+  float StateOfCharge[NO_OF_READINGS] = {23,34,67};
+  float expectedoutput[3][2] = {{20,23}, {25,34},{45,67}};
+
+TEST_CASE("Tests to check if data is correctly read from file") {
+  ReadParametersFromFile(Temperature,StateOfCharge);
+  for(int i=0;i<3;i++)
+  {
+    REQUIRE(Temperature[i] == expectedoutput[i][0]);
+    REQUIRE(StateOfCharge[i] == expectedoutput[i][1]);
+    }
+}
+
+TEST_CASE("Tests to check if all the data is printed correctly from file") {
+ REQUIRE (PrintParameterToConsole(Temperature,StateOfCharge) == 1)
   }
